23b181d87ae95cdb3bcdf95a63b9a9a656aa3906
leandroluizpereira <leandro.2306@hotmail.com>
2021-04-08 11:20:24 -0300

    Update funcoes.js

--- static/javascript/funcoes.js
+++ static/javascript/funcoes.js
@@ -7,5 +7,7 @@ function menuTranslation(divHtml){
 function deletar(){
   document.getElementById("propaganda").style.display = "none";
   document.getElementById("grid-nba").style.gridTemplateColumns = "1fr";
+  document.getElementById("grid-nba").style.marginRight ="100px";
+  document.getElementById("grid-nba").style.marginLeft ="100px";
   console.log("Deu certo!");
 }
