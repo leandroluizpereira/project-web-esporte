0e18d8b570c37e4a2c229b1b6bea684ea0013d60
leandroluizpereira <leandro.2306@hotmail.com>
2021-04-07 23:35:38 -0300

    style

--- static/javascript/funcoes.js
+++ static/javascript/funcoes.js
@@ -1,15 +1,11 @@
 
 function menuTranslation(divHtml){
-
    div= document.getElementById(divHtml).style.left="100px";
-
  return  div;
 }
 
 function deletar(){
   document.getElementById("propaganda").style.display = "none";
-
   document.getElementById("grid-nba").style.gridTemplateColumns = "1fr";
-
-console.log("deu certo!");
+  console.log("Deu certo!");
 }
