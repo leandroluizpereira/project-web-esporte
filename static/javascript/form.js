37738467f3cd8d69897482d7759e0638c53a24cf
leandroluizpereira <leandro.2306@hotmail.com>
2021-04-09 20:32:13 -0300

    style

--- static/javascript/form.js
+++ static/javascript/form.js
@@ -1,14 +1,34 @@
 
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDF_xO5zi0cfP-OXhETSgfkFtNyFGe01H8",
+    authDomain: "project-web-f8ff4.firebaseapp.com",
+    projectId: "project-web-f8ff4",
+    storageBucket: "project-web-f8ff4.appspot.com",
+    messagingSenderId: "1036257551764",
+    appId: "1:1036257551764:web:ee336051c9bd4995329b81"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
- function signIn(){
-   var email = document.getElementById('email').value;
-   var password = document.getElementById('password').value;
-   if (email == ""){
-    alert("O e-mail deve ser preenchido!");
-   }
-   else{
-    alert("E-mail Cadastrado com sucesso!!!"+email);
-    auth.createUserWithEmailAndPassword(email,password);
-   }
- }
+  const auth = firebase.auth();
 
+   function teste(){
+      var email = document.getElementById("email");
+      var password = document.getElementById("password");
+      const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
+      promissse.catch(e => alert(e.message));
+      alert("Signed up");
+   }
+ 
+   function teste2(){
+    var email = document.getElementById("email").value;
+
+    if (email == ""){
+     alert("O e-mail deve ser preenchido!");
+    }
+    else{
+     alert("Signed in");
+    }
+   }
+    
