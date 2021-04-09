a5e6de87368f2aa3593a0fa6523ae11a311ea8e6
leandroluizpereira <leandro.2306@hotmail.com>
2021-04-09 18:15:59 -0300

    javascript

--- static/javascript/form.js
+++ static/javascript/form.js
@@ -1,24 +1,13 @@
 
 
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyB2g-y_9gpMwgenBEzG_7aXFwgmUcR2HHc",
-    authDomain: "project-web-6daa8.firebaseapp.com",
-    projectId: "project-web-6daa8",
-    storageBucket: "project-web-6daa8.appspot.com",
-    messagingSenderId: "227416522409",
-    appId: "1:227416522409:web:44c134c5b1bfdad1efb3b8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
  function signIn(){
-   var email = document.getElementById("email").value;
+   var email = document.getElementById('email').value;
+   var password = document.getElementById('password').value;
    if (email == ""){
     alert("O e-mail deve ser preenchido!");
    }
    else{
-   alert("E-mail Cadastrado com sucesso!!!"+email);
+    alert("E-mail Cadastrado com sucesso!!!"+email);
+    auth.createUserWithEmailAndPassword(email,password);
    }
  }
