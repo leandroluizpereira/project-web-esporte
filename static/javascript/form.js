280328487518da315cb21772e71f2f9b40fec7b5
leandroluizpereira <leandro.2306@hotmail.com>
2021-04-08 23:20:55 -0300

    javascript form

---
+++ static/javascript/form.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDfoIt6xZIOcvGxihOFAMeAk2l_s_24bRg",
+    authDomain: "project-web-form-86db1.firebaseapp.com",
+    projectId: "project-web-form-86db1",
+    storageBucket: "project-web-form-86db1.appspot.com",
+    messagingSenderId: "770199029775",
+    appId: "1:770199029775:web:83c33ccf9ccc7e87a77b7a",
+    measurementId: "G-G74DP6RHHK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
