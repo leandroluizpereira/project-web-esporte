3647c669d37cf2c5395bc5dd73b08ab93171ed8a
leandroluizpereira <leandro.2306@hotmail.com>
2021-04-11 20:51:39 -0300

    style

--- static/javascript/form.js
+++ static/javascript/form.js
@@ -3,13 +3,33 @@
       email = document.getElementById("email").value;
       var email = localStorage.setItem("email",email);
       nome = document.getElementById("nome").value;
-      var nome = localStorage.setItem("nome",email);
-      alert("o email "+localemail);
+      var nome = localStorage.setItem("nome",nome);
+      bairro=document.getElementById("bairro").value;
+      var bairro=localStorage.setItem("bairro",bairro);
+      endereco=document.getElementById("endereco").value;
+      var endereco=localStorage.setItem("endereco",endereco);
+      numero=document.getElementById("numero").value;
+      var numero=localStorage.setItem("numero",numero);
+      telefone=document.getElementById("telefone").value;
+      var telefone =localStorage.setItem("telefone",telefone);
+      celular=document.getElementById("celular").value;
+      var celular=localStorage.setItem("celular",celular);
+      password=document.getElementById("password").value;
+      var password= localStorage.setItem("password",password);
+      confSenha=document.getElementById("confSenha").value;
+      var confSenha=localStorage.setItem("confSenha",confSenha);
+      alert("o email");
+      
 
+      
+    
     }
     function ler(){
 
-      var localemail = localStorage.getItem ("email","email");
+      var localemail = localStorage.getItem("email",+localemail);
+
+
+
      /* document.onchange.savar(); */
 
     }
