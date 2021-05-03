f5c0bbdb2fcb36bd419aac56a3920e81090dfb21
leandroluizpereira <leandro.2306@hotmail.com>
2021-05-03 18:24:49 -0300

    script animaçâo scroll

--- static/javascript/funcoes.js
+++ static/javascript/funcoes.js
@@ -22,7 +22,33 @@ function intervaloSplash(){
   }, 5000);
 }
 
-var elmnt = document.getElementById("")
+window.onscroll = function() {myFunction()};
 
+function myFunction() {
+
+
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("h1").style.animation= "lateral  .3s linear";
+    document.getElementById("grid").style.animation= "lateral  .5s linear";
+  }
+  if (document.body.scrollTop > 4050 || document.documentElement.scrollTop > 4050) {
+    document.getElementById("h1-surf").style.animation= "lateral  .3s linear";
+    document.getElementById("grid2").style.animation= "lateral  .5s linear";
+
+  }
+
+  if (document.body.scrollTop > 6050 || document.documentElement.scrollTop > 6050) {
+    document.getElementById("h1-futebol").style.animation= "lateral  .3s linear";
+    document.getElementById("grid3").style.animation= "lateral  .5s linear";
+    
+  }
+  
+  if (document.body.scrollTop > 8750 || document.documentElement.scrollTop > 8750) {
+    document.getElementById("h1-motor").style.animation= "lateral  .3s linear";
+    document.getElementById("grid4").style.animation= "lateral  .5s linear";
+    
+  }
+  
+}
 
 
